4dccae2a9f007a1a4459c52e6a1df64e829415ac
guilherme-granja <guilherme.marcal@soluti.com.br>
2025-12-08 21:26:52 +0000

    fix: :bug: Including FK in kyc document files migration

--- database/migrations/2025_12_08_144753_create_kyc_document_files_table.php
+++ database/migrations/2025_12_08_144753_create_kyc_document_files_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\KycDocument;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -9,6 +10,7 @@ return new class extends Migration {
     {
         Schema::create('kyc_document_files', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(KycDocument::class)->constrained();
             $table->ulid('object_key')->unique();
             $table->string('bucket_name');
             $table->string('original_filename');
