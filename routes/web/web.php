d3c2e0cca9df7c4764d81932f326788947e54e2a
guilherme-granja <guilherme.marcal@soluti.com.br>
2025-12-23 11:42:39 +0000

    refactor: :truck: Move route files to `web` directory and update imports
    - Relocate route files (`dashboard.php`, `settings.php`, `onboarding.php`) to `/routes/web` for better organization.
    - Update `web.php` and `app.php` to reflect new file paths.

--- routes/web/web.php
+++ routes/web/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('', HomePageController::class)->name('home');
 
-require __DIR__.'/onboarding.php';
-require __DIR__.'/dashboard.php';
-require __DIR__.'/settings.php';
+require __DIR__ . '/onboarding.php';
+require __DIR__ . '/dashboard.php';
+require __DIR__ . '/settings.php';
